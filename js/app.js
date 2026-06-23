7d353efa0795276cbc68bc95a2bb47aa22594aea
WA-2211 <walaaalghanmi@gmail.com>
2026-06-23 10:56:04 +0300

    Create rest Button

--- js/app.js
+++ js/app.js
@@ -3,11 +3,11 @@ const resultDisplayElement = document.querySelector('#result-display')
 const rockElement = document.querySelector('#rock')
 const paperElement = document.querySelector('#paper')
 const scissorElement = document.querySelector('#scissors')
-
+const resetElement = document.querySelector('#reset')
 
 
 /*-------------------------------- Constants --------------------------------*/
-const choices = ['rock', 'paper', 'scissors']
+const choices = ['rock', 'paper', 'scissors', 'reset']
 console.log(choices)
 
 /*-------------------------------- Variables --------------------------------*/
@@ -20,16 +20,18 @@ let message
 function play(event){
     //1. Identify what choice I picked
     getPlayerChoice(event)
-    console.log('Player Choice:' ,playerChoice)
     //2. Randomly pick computer choice
     getComputerChoice()
-    console.log('Compuetr Choice:', computerChoice)
 
     //3. compare my choice vs computer choice
     compareChoices()
     console.log(message)
+
     //4. display winner
-    resultDisplayElement.textContent = message
+    resultDisplayElement.innerHTML = `<div style = text-align:center>${message}<br> You picked ${playerChoice} and Computer picked ${computerChoice}</div>`
+    console.log('Player Choice:' ,playerChoice)
+    console.log('Compuetr Choice:', computerChoice)
+
 }
 
 function getPlayerChoice(event){
@@ -59,13 +61,25 @@ function compareChoices(){
         message = 'You Win!'
 
     }
+    else if (playerChoice === ''){
+
+    }
     else {
         message = 'You lose :( '
     }
   
 }
 
+
+function resetChoices(){
+    playerChoice = null
+    computerChoice = null
+    message = ''
+     resultDisplayElement.innerHTML = ' '
+   
+}
 /*----------------------------- Event Listeners -----------------------------*/
 rockElement.addEventListener('click', play)
 paperElement.addEventListener('click', play)
 scissorElement.addEventListener('click', play)
+resetElement.addEventListener('click', resetChoices)
