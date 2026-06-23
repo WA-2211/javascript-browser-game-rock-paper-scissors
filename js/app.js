91a5491c0c09ee94dc685e9d9e6c1019478a82f6
WA-2211 <walaaalghanmi@gmail.com>
2026-06-23 09:31:52 +0300

    add chached refrences, variables, and constants

--- js/app.js
+++ js/app.js
@@ -1,10 +1,14 @@
 /*------------------------ Cached Element References ------------------------*/
-
+const resultDisplayElement = document.querySelector('#result-display')
 
 /*-------------------------------- Constants --------------------------------*/
+const choices = ['rock', 'paper', 'scissors']
+console.log(choices)
 
 /*-------------------------------- Variables --------------------------------*/
-
+let playerChoice
+let computerChoice
+let msg
 
 /*-------------------------------- Functions --------------------------------*/
 
