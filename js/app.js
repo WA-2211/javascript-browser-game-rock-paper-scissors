f775b29ca31fe520a667c610929573e98f08782c
WA-2211 <walaaalghanmi@gmail.com>
2026-06-23 09:50:54 +0300

    Take user choice

--- js/app.js
+++ js/app.js
@@ -1,5 +1,10 @@
 /*------------------------ Cached Element References ------------------------*/
 const resultDisplayElement = document.querySelector('#result-display')
+const rockElement = document.querySelector('#rock')
+const paperElement = document.querySelector('#paper')
+const scissorElement = document.querySelector('#scissors')
+
+
 
 /*-------------------------------- Constants --------------------------------*/
 const choices = ['rock', 'paper', 'scissors']
@@ -8,8 +13,22 @@ console.log(choices)
 /*-------------------------------- Variables --------------------------------*/
 let playerChoice
 let computerChoice
-let msg
+let message
 
 /*-------------------------------- Functions --------------------------------*/
 
+function play(event){
+    getPlayerChoice(event)
+    console.log(playerChoice)
+
+}
+
+function getPlayerChoice(event){
+    playerChoice = event.target.id
+
+}
+
 /*----------------------------- Event Listeners -----------------------------*/
+rockElement.addEventListener('click', play)
+paperElement.addEventListener('click', play)
+scissorElement.addEventListener('click', play)
